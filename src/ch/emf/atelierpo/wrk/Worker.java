e33e822d78e3a92ee0aa89bc23f9f54e09860f5d
Romarin <Romarin@DESKTOP-QLHCFVG>
2023-03-28 09:25:46 +0200

    Ajout de la spécification de la JavaDoc pour la méthode : "controlePlaqueImmatriculation"

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -6,8 +6,22 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-   
+    /**
+     * Vérifie si une plaque d'immatriculation est valide pour un canton suisse
+     * spécifié.
+     *
+     * @param canton le canton de la plaque d'immatriculation, représenté par
+     * une chaîne de caractères contenant uniquement des lettres majuscules A-Z
+     * 
+     * @param numeroPlaque le numéro de la plaque d'immatriculation, représenté
+     * par une chaîne de caractères contenant uniquement des chiffres
+     * 
+     * @return true si la plaque d'immatriculation est valide pour le canton
+     * spécifié, false sinon
+     */
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
+        // Implémentation de la logique de validation de la plaque d'immatriculation pour le canton spécifié
+
         return false;
     }
 
